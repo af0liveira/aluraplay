dd961b449251650438c7d4ef304a5afb583239c5
af0liveira <af0liveira@icloud.com>
2023-08-31 18:25:43 +0200

    Implement class for song tracks (SongTrack).

---
+++ src/com/github/af0liveira/aluraplay/modelos/SongTrack.java
@@ -0,0 +1,48 @@
+package com.github.af0liveira.aluraplay.modelos;
+
+public class SongTrack extends Audio {
+    private String artist;
+    private String album;
+    private String composer;
+    private String genre;
+    prinvate int year;
+
+    private String getArtist() {
+        return this.artist;
+    }
+
+    private void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    private String getAlbum() {
+        return this.album;
+    }
+
+    private void setAlbum(String album) {
+        this.album = album;
+    }
+
+    private String getComposer() {
+        return this.composer;
+    }
+
+    private void setComposer(String composer) {
+        this.composer = composer;
+    }
+
+    private String getGenre() {
+        return this.genre;
+    }
+    private void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
